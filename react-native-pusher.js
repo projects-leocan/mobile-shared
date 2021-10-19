6a62e69e40c37abd5f21718d84bd36cfe8d064a9
Macmini <miralgaywala@gmail.com>
2021-10-19 11:10:40 +0530

    - add function for pusher push notification

--- react-native-pusher.js
+++ react-native-pusher.js
@@ -3,18 +3,18 @@
 const { NativeModules, Platform, Alert } = require('react-native');
 const DeviceInfo = require('react-native-device-info');
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import RNPusherPushNotifications from 'react-native-pusher-push-notifications';
 
+const Pusher = NativeModules.RNPusher;
 
-const Pusher = NativeModules.RNPusher ;
-
-const nativePusher = function(options) {
-  const { deviceId, pusherKey } = options;
+const nativePusher = function (options) {
+  const { deviceId, pusherKey, PUSHER_BEAMS_INSTANCE_ID } = options;
   const onRegister = (deviceToken) => {
     Pusher.registerWithDeviceToken(deviceToken);
   };
   const onRegistrationError = (error) => {
     console.error(error);
-    Alert.alert('PN Error', error, [{text: 'OK', onPress: () => console.log('OK Pressed')},]);
+    Alert.alert('PN Error', error, [{ text: 'OK', onPress: () => console.log('OK Pressed') },]);
   };
 
   return {
@@ -55,14 +55,45 @@ const nativePusher = function(options) {
 
     unsubscribe(interest) {
       if (Platform.OS === 'ios' && DeviceInfo.isEmulator()) { return; }
-      
+
       try {
         Pusher.unsubscribe(interest);
       } catch (error) {
         console.log(error)
       }
+    },
+
+    subscribePPN(interest) {
+      RNPusherPushNotifications.subscribe(
+        interest,
+        (statusCode, response) => {
+          console.log(statusCode, response);
+        },
+        () => {
+          console.log('Push Notifications Success');
+        }
+      );
+    },
+
+    unsubscribePPN(interest) {
+      RNPusherPushNotifications.unsubscribe(
+        interest,
+        (statusCode, response) => {
+          console.log(statusCode, response);
+        },
+        () => {
+          console.log('Success');
+        }
+      );
+    },
+
+    subscriptionsChangedListener() {
+      return RNPusherPushNotifications.setOnSubscriptionsChangedListener((interests) => {
+        return interests;
+      });
     }
   };
 }
 
-module.exports = nativePusher;
+// module.exports = nativePusher;
+export default nativePusher;
